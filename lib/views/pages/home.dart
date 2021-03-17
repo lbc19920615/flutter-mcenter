cffc7db5fd7d00af3f3fac1ed1cf27b0adc5b029
lbc19920615 <1585638808@qq.com>
2021-03-17 11:20:58 +0800

    update

--- lib/views/pages/home.dart
+++ lib/views/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lbc_mcenter/utils/constants.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,28 +10,90 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<String> _suggestions = <String>[];
+  final Set<String> _saved = new Set<String>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Constants.appName),
+        actions: <Widget>[
+          // 新增代码开始 ...
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
-      body: Container(
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return new ListView.builder(
         padding: EdgeInsets.all(Constants.commonPadding),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:'),
-          ],
-        ),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.add(uuid.v1());
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(String suggestion) {
+    final bool alreadySaved = _saved.contains(suggestion); // 新增本行
+    return new ListTile(
+      title: new Text(
+        suggestion,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        // 新增代码开始 ...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(suggestion);
+          } else {
+            _saved.add(suggestion);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (String suggestion) {
+              return new ListTile(
+                title: new Text(
+                  suggestion,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return new Scaffold(
+            // 新增 6 行代码开始 ...
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          ); // ... 新增代码段结束.
+        },
       ),
     );
   }
